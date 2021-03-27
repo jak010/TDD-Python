102245fd0abaffed9367af79c1f72fee9f9e47dc
jak010 <bluetoon@naver.com>
2021-03-27 20:24:46 +0900

    최소 html 반환하는 뷰

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+
+from lists import views
+from django.conf.urls import url
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # path(r"^", views.home_page)
+    url(r"^$", views.home_page)
 ]
