4f782b89c68fd44b7ff9f628444de717a586d08f
jak010 <bluetoon@naver.com>
2021-03-27 22:54:15 +0900

    test_home_page_returns_correct_html -> refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
@@ -15,6 +15,11 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswidth(b"<html>"))
-        self.assertIn("b<title>To-Do lists</title>", response.content)
-        self.assertTrue(response.content.endswith(b"</html>"))
+
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        # self.assertIn(b"<title>To-Do lists</title>", response.content)
+        # self.assertTrue(response.content.decode().endswith('</html>'))
+
+        # Refactoring
+        exepected_html = render_to_string("home.html")
+        self.assertEqual(response.content.decode(),exepected_html)
