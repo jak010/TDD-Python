052a2f9cd3bfe4466dea980f8cd021bb13577531
jak010 <bluetoon@naver.com>
2021-04-13 22:04:17 +0900

    ~page116

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 
-# Create your models here.
+class List(models.Model):
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
+
 
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.SET_DEFAULT, default=None, blank=True, null=True)
 
+    class Meta:
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from .models import Item
-
+from .models import Item,List
 
 # Create your tests here.
 
@@ -67,28 +66,47 @@ class HomePageTest(TestCase):
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        # first_item = Item()
+        # first_item.text = '첫 번째 아이템'
+        # first_item.save()
+
+        list_ = List()
+        list_.save()
+
         first_item = Item()
-        first_item.text = '첫 번째 아이템'
+        first_item.text = "첫 번째 아이템"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
 
         saved_items = Item.objects.all()
+
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
+
         self.assertEqual(second_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
 
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text="itemey 1", list=list_)
+        Item.objects.create(text="itemey 2", list=list_)
+
     def test_uses_list_template(self):
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertTemplateUsed(response, "list.html")
+
 
     def test_home_page_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
@@ -98,6 +116,7 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
 
     def test_saving_a_POST_request(self):
         self.client.post(
